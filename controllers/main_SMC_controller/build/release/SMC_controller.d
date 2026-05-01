ba21ada8531df0910f4caf677f222e7a991d0024
bthu <bthu.fablab@gmail.com>
2026-05-01 16:26:10 +0700

    style: cleaning code

--- controllers/main_SMC_controller/build/release/SMC_controller.d
+++
@@ -1,2 +0,0 @@
-build/release/SMC_controller.o: SMC_controller.c SMC_controller.h \
- rtwtypes.h tmwtypes.h
--- controllers/main_SMC_controller/build/release/SMC_controller_initialize.d
+++
@@ -1,2 +0,0 @@
-build/release/SMC_controller_initialize.o: SMC_controller_initialize.c \
- SMC_controller_initialize.h rtwtypes.h tmwtypes.h
